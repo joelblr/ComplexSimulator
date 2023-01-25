f214032db30ac79fddca1c878867af7169ccaf45
joelblr <95953794+joelblr@users.noreply.github.com>
2023-01-26 00:43:26 +0530

    Create BarodaBank.java

---
+++ banking/barodaBank/BarodaBank.java
@@ -0,0 +1,41 @@
+package banking.barodaBank;
+
+import banking.*;
+
+import java.util.*;
+public class BarodaBank implements Bank {
+
+
+	public static void createAccount(LinkedList<Bank> accList) {
+		
+
+	}
+	public static void deleteAccount(LinkedList<Bank> accList) throws IllegalAccessException {
+		
+
+	}
+	public static void switchAccount(LinkedList<Bank> accList) throws IllegalAccessException {
+		
+
+	}
+
+
+	public static void loanInterest(Bank acc) {
+		
+
+	}
+	public static void amountBalance(Bank acc) {
+		
+
+	}
+	public static void amountDeposit(Bank acc) throws UnsupportedOperationException {
+		
+
+	}
+	public static void amountWithdraw(Bank acc) throws UnsupportedOperationException {
+		
+
+	}
+
+
+}
