9a8ba68d7fa4b5949427e0ad4daf345f948bf827
joelblr <95953794+joelblr@users.noreply.github.com>
2023-02-07 19:53:40 +0530

    Add files via upload

--- main/SuperMain.java
+++ main/SuperMain.java
@@ -23,12 +23,6 @@ public class SuperMain {
 
 
 
-	public final static void showErrorMessage(String msg) {
-//		Design.printBox(new String(Character.toChars(0x2757))+" "+msg);
-		Design.printBox(msg);
-	}
-
-
 	public static void showMenu() {
 
 		List<String> stdin = 
@@ -46,40 +40,6 @@ public class SuperMain {
 	}
 
 
-	public final static void loadingProcess(int time) {
-
-		String a = "|/-\\";
-		long start = System.currentTimeMillis();
-		try {
-			int i = 0;
-			Design.printBox("Loading your Choice   ");
-			System.out.print("\033[2A\033["+(8+4+21)+"C");
-			while (true) {
-				
-				System.out.print(a.charAt(i) + " ");
-				Thread.sleep(150);
-				System.out.print("\b\b");
-				long now = System.currentTimeMillis();
-				if (now - start >= time) break;
-				i = (i+1) % 4;
-			}
-
-		}catch(Exception e) {}
-		finally {
-			System.out.print("\033[1B");
-			for (int i = 0; i < 3; i++) {
-				System.out.print("\033[2K");
-				System.out.print("\033[1A");
-			}
-		}
-		System.out.println();
-	}
-
-
-	public final static void continueProcess() {
-		Design.printBox("PRESS <ENTER> KEY TO CONTINUE... $");
-	}
-
 
 	public static void run() {
 		return;
@@ -94,34 +54,27 @@ public class SuperMain {
 
 				Design.clearScreen();
 				if (errorFlag) {
-					showErrorMessage(errorMsg);
+					Design.showErrorMessage(errorMsg);
 					errorFlag = false;
 				}
 				showMenu();
-				loadingProcess(1500);
+				Design.loadingProcess(1500);
 
 				switch (getChoice) {
 
-//					case 0 :
-//						break;
-
 					case 1 :
-//						errorMsg = "WORK-IN-PROGRESS, PLZ WAIT FOR A FEW WEEKS...";  errorFlag = true;
 						CalciMain.run();
 						break;
 
 					case 2 :
-//						errorMsg = "WORK-IN-PROGRESS, PLZ WAIT FOR A FEW WEEKS...";  errorFlag = true;
-						EmploMain.run();
+						EmployeeMain.run();
 						break;
 
 					case 3 :
-//						errorMsg = "WORK-IN-PROGRESS, PLZ WAIT FOR A FEW WEEKS...";  errorFlag = true;
 						BescomMain.run();
 						break;
 
 					case 4 :
-//						errorMsg = "WORK-IN-PROGRESS, PLZ WAIT FOR A FEW WEEKS...";  errorFlag = true;
 						BankMain.run();
 						break;
 
@@ -140,7 +93,7 @@ public class SuperMain {
 
 			}catch (NumberFormatException e) {
 				System.out.println();
-				loadingProcess(500);
+				Design.loadingProcess(600);
 				errorMsg = "<INVALID INPUT!>";	errorFlag = true;
 
 			}
